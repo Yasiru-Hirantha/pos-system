5b94d2572df58302d1cec9e8f4657d14d3cb3ef8
yasiru <yasiru.hirantha@gmail.com>
2023-10-31 00:41:05 +0530

    Finalize the deleteCustomer functionality

--- front-end/js/manage-customers.js
+++ front-end/js/manage-customers.js
@@ -154,14 +154,14 @@ function getCustomers(){
                         <td>
                             <div class="actions d-flex gap-3 justify-content-center">
                                 <svg data-bs-toggle="tooltip" data-bs-title="Edit Customer" xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor"
-                                    class="bi bi-pencil-square" viewBox="0 0 16 16">
+                                    class="bi bi-pencil-square edit" viewBox="0 0 16 16">
                                     <path
                                         d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z" />
                                     <path fill-rule="evenodd"
                                         d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z" />
                                 </svg>
                                 <svg data-bs-toggle="tooltip" data-bs-title="Delete Customer" xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor"
-                                    class="bi bi-trash" viewBox="0 0 16 16">
+                                    class="bi bi-trash delete" viewBox="0 0 16 16">
                                     <path
                                         d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6Z" />
                                     <path
@@ -172,6 +172,8 @@ function getCustomers(){
                     </tr>
                 `);
                 });
+                const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+                const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl));
                 if (!customerList.length){
                     $("#tbl-customers tfoot").show();
                 }else {
@@ -215,3 +217,22 @@ function showProgress(xhr){
         setTimeout(()=> progressBar.width('0%'), 500);
     });
 }
+
+tbodyElm.on('click', ".delete", (eventData)=> {
+    /* XHR -> jQuery AJAX */
+    const id = +$(eventData.target).parents("tr").children("td:first-child").text().replace('C', '');
+    const xhr = new XMLHttpRequest();
+    const jqxhr = $.ajax(`http://localhost:8080/pos/customers/${id}`, {
+        method: 'DELETE',
+        xhr: ()=> xhr           // This is a hack to obtain the xhr that is used by jquery
+    });
+    showProgress(xhr);
+    jqxhr.done(()=> {
+        showToast('success', 'Deleted', 'Customer has been deleted successfully');
+        $(eventData.target).tooltip('dispose');
+        getCustomers();
+    });
+    jqxhr.fail(()=> {
+        showToast('error', 'Failed', "Failed to delete the customer, try again!");
+    });
+});
