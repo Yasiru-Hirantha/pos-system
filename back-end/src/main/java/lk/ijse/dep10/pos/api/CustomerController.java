6ab836f80bee389e1e5385455f3fd86f8aa9ddf7
yasiru <yasiru.hirantha@gmail.com>
2023-10-30 15:52:40 +0530

    Expose the getCustomers() API

--- back-end/src/main/java/lk/ijse/dep10/pos/api/CustomerController.java
+++ back-end/src/main/java/lk/ijse/dep10/pos/api/CustomerController.java
@@ -4,11 +4,14 @@ import lk.ijse.dep10.pos.dto.CustomerDTO;
 import lk.ijse.dep10.pos.dto.ResponseErrorDTO;
 import org.apache.commons.dbcp2.BasicDataSource;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/customers")
@@ -18,8 +21,38 @@ public class CustomerController {
     @Autowired
     private BasicDataSource pool;
 
+    // GET http://localhost:8080/pos/customers?q=Galle
+    @GetMapping
+    public Object getCustomers(@RequestParam(value = "q", required = false) String query) {
+        if (query == null) query = "";
+        try (Connection connection = pool.getConnection()) {
+            PreparedStatement stm = connection.prepareStatement
+                    ("SELECT * FROM customer WHERE id LIKE ? OR name LIKE ? OR address LIKE ? OR contact LIKE ?");
+            query = "%" + query + "%";
+            for (int i = 1; i <= 4; i++) {
+                stm.setString(i, query);
+            }
+            ResultSet rst = stm.executeQuery();
+            List<CustomerDTO> customerList = new ArrayList<>();
+            while (rst.next()) {
+                int id = rst.getInt("id");
+                String name = rst.getString("name");
+                String address = rst.getString("address");
+                String contact = rst.getString("contact");
+                customerList.add(new CustomerDTO(id, name, address, contact));
+            }
+            HttpHeaders headers = new HttpHeaders();
+            headers.add("X-Count", customerList.size() + "");
+            return new ResponseEntity<>(customerList, headers, HttpStatus.OK);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return new ResponseEntity<>(new ResponseErrorDTO(500, e.getMessage()),
+                    HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
     @PostMapping
-    public ResponseEntity<?> saveCustomer(@RequestBody CustomerDTO customer){
+    public ResponseEntity<?> saveCustomer(@RequestBody CustomerDTO customer) {
         try {
             Thread.sleep(3000);
         } catch (InterruptedException e) {
@@ -39,11 +72,11 @@ public class CustomerController {
             customer.setId(id);
             return new ResponseEntity<>(customer, HttpStatus.CREATED);
         } catch (SQLException e) {
-            if (e.getSQLState().equals("23000")){
+            if (e.getSQLState().equals("23000")) {
                 return new ResponseEntity<>(
-                        new ResponseErrorDTO(HttpStatus.CONFLICT.value(),e.getMessage()),
+                        new ResponseErrorDTO(HttpStatus.CONFLICT.value(), e.getMessage()),
                         HttpStatus.CONFLICT);
-            }else{
+            } else {
                 return new ResponseEntity<>(
                         new ResponseErrorDTO(500, e.getMessage()),
                         HttpStatus.INTERNAL_SERVER_ERROR);
