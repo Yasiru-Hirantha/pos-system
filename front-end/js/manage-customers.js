5d2d9faad9da24ff687b51d2aa21aa06b92299bf
yasiru <yasiru.hirantha@gmail.com>
2023-10-30 16:23:58 +0530

    Finsih consuming getCustomers web service

--- front-end/js/manage-customers.js
+++ front-end/js/manage-customers.js
@@ -159,3 +159,64 @@ function showToast(toastType, header, message) {
     $("#toast .toast-body").text(message);
     toast.toast('show');
 }
+
+function getCustomers(){
+    const xhr = new XMLHttpRequest();
+
+    xhr.addEventListener('readystatechange', ()=> {
+        if (xhr.readyState === 4){
+            if (xhr.status === 200){
+                tbodyElm.empty();
+                const customerList = JSON.parse(xhr.responseText);
+                customerList.forEach(customer => {
+                    tbodyElm.append(`
+                    <tr>
+                        <td class="text-center">${formatCustomerId(customer.id)}</td>
+                        <td>${customer.name}</td>
+                        <td class="d-none d-xl-table-cell">${customer.address}</td>
+                        <td class="contact text-center">${customer.contact}</td>
+                        <td>
+                            <div class="actions d-flex gap-3 justify-content-center">
+                                <svg data-bs-toggle="tooltip" data-bs-title="Edit Customer" xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor"
+                                    class="bi bi-pencil-square" viewBox="0 0 16 16">
+                                    <path
+                                        d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z" />
+                                    <path fill-rule="evenodd"
+                                        d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z" />
+                                </svg>
+                                <svg data-bs-toggle="tooltip" data-bs-title="Delete Customer" xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor"
+                                    class="bi bi-trash" viewBox="0 0 16 16">
+                                    <path
+                                        d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6Z" />
+                                    <path
+                                        d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1ZM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118ZM2.5 3h11V2h-11v1Z" />
+                                </svg>
+                            </div>
+                        </td>
+                    </tr>
+                `);
+                });
+                if (!customerList.length){
+                    $("#tbl-customers tfoot").show();
+                }else {
+                    $("#tbl-customers tfoot").hide();
+                }
+            }else{
+                tbodyElm.empty();
+                $("#tbl-customers tfoot").show();
+                showToast('error', 'Failed', 'Failed to fetch customers');
+                console.log(JSON.parse(xhr.responseText));
+            }
+        }
+    });
+
+    const searchText = $("#txt-search").val().trim();
+    const query = (searchText) ? `?q=${searchText}`: "";
+
+    xhr.open('GET', 'http://localhost:8080/pos/customers' + query, true);
+
+    xhr.send();
+}
+
+getCustomers();
+$("#txt-search").on('input', ()=> getCustomers());
