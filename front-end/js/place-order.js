2233f28c0e70d07cd42f2d5388399371ba83ba13
yasiru <yasiru.hirantha@gmail.com>
2023-11-01 00:31:52 +0530

    Finalize customer section of the place order function

--- front-end/js/place-order.js
+++ front-end/js/place-order.js
@@ -1,7 +1,52 @@
+// import {LocalDateTime, DateTimeFormatter} from '@js-joda/core';
+import {DateTimeFormatter, LocalDateTime} from '../node_modules/@js-joda/core/dist/js-joda.esm.js';
 
-for(let i = 0; i < 15; i++){
-    const tr = $("#tbl-order tbody tr:first-child").clone();
-    $("#tbl-order tbody").append(tr);
+/* Module Level Variables, Constants */
+const API_BASE_URL = 'http://localhost:8080/pos';
+const orderDateTimeElm = $("#order-date-time");
+const tbodyElm = $("#tbl-order tbody");
+const txtCustomer = $("#txt-customer");
+let customer = null;
+
+/* Initialization Logic */
+setDateTime();
+tbodyElm.empty();
+
+/* Event Handlers & Timers */
+setInterval(setDateTime, 1000);
+txtCustomer.on('input', () => findCustomer());
+txtCustomer.on('blur', ()=> {
+    if (txtCustomer.val() && !customer){
+        txtCustomer.addClass("is-invalid");
+    }
+});
+$("#btn-clear-customer").on('click', ()=> {
+    customer = null;
+    $("#customer-name").text("Walk-in Customer");
+    txtCustomer.val("");
+    txtCustomer.removeClass("is-invalid");
+    txtCustomer.trigger("focus");
+});
+
+/* Functions */
+function setDateTime() {
+    const now = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+    orderDateTimeElm.text(now);
 }
 
-$("#tbl-order tbody").empty();
+function findCustomer() {
+    const customerNameElm = $("#customer-name");
+    const idOrContact = txtCustomer.val().trim().replace('C', '');
+
+    txtCustomer.removeClass("is-invalid");
+    if (!idOrContact) return;
+    const jqxhr = $.ajax(`${API_BASE_URL}/customers/${idOrContact}`);
+    jqxhr.done((data)=>{
+        customer = data;
+        customerNameElm.text(customer.name);
+    });
+    jqxhr.fail(()=>{
+        customer = null;
+        customerNameElm.text("Walk-in Customer");
+    });
+}
